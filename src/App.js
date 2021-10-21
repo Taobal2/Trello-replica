18b90b649c3f3f79757a35cd451e63a4747bf072
Balogun Taoheed <baloguntaoheed2@gmail.com>
2021-10-21 07:50:36 -0700

    a test commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Trello from './Trello/Trello'
-import {Api} from './ApiData/Api'
 
 const App = () => {
   return (
     <div>
       <Trello />
-      {/* <Api /> */}
     </div>
   )
 }
